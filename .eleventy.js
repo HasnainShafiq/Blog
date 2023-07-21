7ca2c3da1a4c53fadea9b3229eb26dbb0aec022f
Uncreative-creativity <31780746+HasnainShafiq@users.noreply.github.com>
2023-07-21 17:12:31 +0100

    Upgraded to Eleventy 2

--- .eleventy.js
+++ .eleventy.js
@@ -1,21 +1,26 @@
-const { DateTime } = require('luxon')
+const { DateTime } = require("luxon");
 const yaml = require("js-yaml");
-const Image = require('@11ty/eleventy-img');
+const Image = require("@11ty/eleventy-img");
 
-module.exports = function(eleventyConfig) {
+//other packages
+const UpgradeHelper = require("@11ty/eleventy-upgrade-help");
 
-    eleventyConfig.addPassthroughCopy("./src/assets");
-    eleventyConfig.addPassthroughCopy("./src/css");
-    eleventyConfig.addPassthroughCopy("./src/admin");
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("./src/assets");
+  eleventyConfig.addPassthroughCopy("./src/css");
+  eleventyConfig.addPassthroughCopy("./src/admin");
 
-    eleventyConfig.addFilter("postDate", (dateObj) => {
-      return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
-    })
+  eleventyConfig.addFilter("postDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+  });
 
-    return {
-      dir: {
-        input: "src",
-        output: "public"
-      }
-    }
+  // It’s important that UpgradeHelper is the LAST addPlugin() call.
+  eleventyConfig.addPlugin(UpgradeHelper);
+
+  return {
+    dir: {
+      input: "src",
+      output: "public",
+    },
   };
+};
